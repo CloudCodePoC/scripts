c7dec1648772fc3b28014b4e7c5df0c6b7c98fd6
P Campbell <pcampbell.edu@gmail.com>
2017-11-23 16:59:55 -0500

    add git to all

--- win-android-dev.ps1
+++ win-android-dev.ps1
@@ -1,7 +1,7 @@
-Write-Host This script will download chocolatey and install 
+Write-Host This script will download chocolatey and install
 Write-Host software to configure this box for Android Development
 Write-Host Afterward you will need to install necessary SDKs & AVDs via Android Studio
-Write-Host 
+Write-Host
 Write-Host You are about to download https://chocolatey.org/install.ps1
 Write-Host Please inspect the URL ^  then confirm or deny installation
 
@@ -15,21 +15,24 @@ if ($yn -ne "y" ) -and ($yn -ne "Y") {
 	}
 
 $Date = Get-Date -format yyyy-M-d
-Write-Host "Config windows on '$Date'" 
+Write-Host "Config windows on '$Date'"
 $logfile = "install-$Date.log"
 
 #@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat #None -ExecutionPolicy Bypass -Command "iex ((New-Object #System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET #"PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-Set-ExecutionPolicy Bypass -Scope Process -Force 
+Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 choco install jdk8  jre8 -y | Out-File  -Append  $logfile
 choco install androidstudio -y	| Out-File  -Append   $logfile
+choco install git -y | Out-File  -Append  $logfile
 
+# misc
 choco install notepadplusplus atom 7zip -y | Out-File  -Append   $logfile
 choco install firefox  -y  | Out-File  -Append   $logfile
+
 # optional install slack
 choco install slack	| Out-File  -Append   $logfile
 
--- win-csharp-dev.ps1
+++ win-csharp-dev.ps1
@@ -1,7 +1,7 @@
-Write-Host This script will download chocolatey and install 
+Write-Host This script will download chocolatey and install
 Write-Host software to configure this box for Android Development
 Write-Host Afterward you will need to install necessary SDKs & AVDs via Android Studio
-Write-Host 
+Write-Host
 Write-Host You are about to download https://chocolatey.org/install.ps1
 Write-Host Please inspect the URL ^  then confirm or deny installation
 
@@ -15,19 +15,20 @@ if ($yn -ne "y" ) -and ($yn -ne "Y") {
 	}
 
 $Date = Get-Date -format yyyy-M-d
-Write-Host "Config windows on '$Date'" 
+Write-Host "Config windows on '$Date'"
 $logfile = "install-$Date.log"
 
 #@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat #None -ExecutionPolicy Bypass -Command "iex ((New-Object #System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET #"PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-Set-ExecutionPolicy Bypass -Scope Process -Force 
+Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-choco install visualstudio2017professional
-choco install dotnet4.7.1  # should be dotnetcore maybe?
-choco install git
+choco install visualstudio2017professional -y | Out-File  -Append   $logfile
+choco install dotnet4.7.1  -y | Out-File  -Append   $logfile
+# should be dotnetcore maybe?
+choco install git -y | Out-File  -Append   $logfile
 
 # misc
 choco install notepadplusplus atom 7zip -y | Out-File  -Append   $logfile
